ec38712a99369704a0631ece7f41d12efa1d65ca
Matt Rowan <matt.rowan121@gmail.com>
2023-05-16 15:50:42 -0400

    Create work history page

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,27 +1,27 @@
 import Pagination from "@/components/Pagination";
 import Link from "next/link";
 
-const summary = () => {
+const work = () => {
   return (
     <div className="mt-16 mx-10 p-4 min-h-screen">
       {/* Back Button */}
-      <Link href="/details">
+      <Link href="/summary">
         <button className="text-white bg-violet-600 stdBtn mb-10">
           Go Back
         </button>
       </Link>
 
       {/* Pagination */}
-      <Pagination completed={1} />
+      <Pagination completed={2} />
 
       {/* Buttons */}
       <div className="flex space-x-3 justify-center items-center">
-        <Link href="/details">
+        <Link href="/summary">
           <button className="text-violet-600 border-[1px] border-violet-600 stdBtn">
             Back
           </button>
         </Link>
-        <Link href="/work">
+        <Link href="">
           <button className="text-white bg-violet-600 stdBtn btnHover">
             Next Step
           </button>
@@ -31,4 +31,4 @@ const summary = () => {
   );
 };
 
-export default summary;
+export default work;
