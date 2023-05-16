32b2a27c1882248f3df03535f54f71891c1031d7
Matt Rowan <matt.rowan121@gmail.com>
2023-05-16 15:52:52 -0400

    Create skills page

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -1,27 +1,27 @@
 import Pagination from "@/components/Pagination";
 import Link from "next/link";
 
-const work = () => {
+const skills = () => {
   return (
     <div className="mt-16 mx-10 p-4 min-h-screen">
       {/* Back Button */}
-      <Link href="/summary">
+      <Link href="/work">
         <button className="text-white bg-violet-600 stdBtn mb-10">
           Go Back
         </button>
       </Link>
 
       {/* Pagination */}
-      <Pagination completed={2} />
+      <Pagination completed={3} />
 
       {/* Buttons */}
       <div className="flex space-x-3 justify-center items-center">
-        <Link href="/summary">
+        <Link href="/work">
           <button className="text-violet-600 border-[1px] border-violet-600 stdBtn">
             Back
           </button>
         </Link>
-        <Link href="/skills">
+        <Link href="">
           <button className="text-white bg-violet-600 stdBtn btnHover">
             Next Step
           </button>
@@ -31,4 +31,4 @@ const work = () => {
   );
 };
 
-export default work;
+export default skills;
